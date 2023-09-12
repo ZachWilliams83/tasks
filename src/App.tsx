585d9ca8daa995f71f8bbe416a9414e17285223a
ZachWilliams83 <zachary.j.williams@maine.edu>
2023-09-12 11:47:23 -0400

    resolved merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+<<<<<<< HEAD
                 Umaine COS420 with React Hooks and TypeScript
+=======
+                UM COS420 with React Hooks and TypeScript
+>>>>>>> d80d3ed2ebcd608ebeab251e3e2404b141f8e46b
             </header>
             <p>Zach Williams</p>
             <p>Hello World</p>
