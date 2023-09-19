c4a4a00df3a0f232336a33daa6c23abfaf640f7d
ZachWilliams83 <zachary.j.williams@maine.edu>
2023-09-18 20:59:20 -0400

    completed array tasks

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-<<<<<<< HEAD
-                Umaine COS420 with React Hooks and TypeScript
-=======
-                UM COS420 with React Hooks and TypeScript
->>>>>>> upstream/task-arrays
+                Umaine COS420 with React Hooks and TypeScript UM COS420 with
+                React Hooks and TypeScript
             </header>
             <h2>This is a header!</h2>
             <img
