265985407ef160e28c46e6b5235f1f4395b2012c
ZachWilliams83 <zachary.j.williams@maine.edu>
2023-10-02 21:18:58 -0400

    complete addNew

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -158,7 +159,8 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const blankQuestion = makeBlankQuestion(id, name, type);
+    return [...questions, blankQuestion];
 }
 
 /***
