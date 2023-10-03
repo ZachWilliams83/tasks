81b42b9b7c53fa15e071673926418562facf2417
ZachWilliams83 <zachary.j.williams@maine.edu>
2023-10-02 21:16:53 -0400

    complete sameType

--- src/nested.ts
+++ src/nested.ts
@@ -143,7 +143,8 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const firstCheck = questions[0].type;
+    return questions.every((q) => q.type === firstCheck);
 }
 
 /***
